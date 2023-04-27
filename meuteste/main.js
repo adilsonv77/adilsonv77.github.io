fbd19c0cf89d37503d23f2e61fded4dd601d3b03
Adilson Vahldick <adilsonv77@gmail.com>
2023-04-27 14:55:48 -0300

    Update main.js

--- meuteste/main.js
+++ meuteste/main.js
@@ -12,11 +12,13 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     const {renderer, scene, camera} = mindarThree;
 
+    // esse eh para nao deixar a figura em branco
     const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
     scene.add(light);
 
-    const raccoon = await loadGLTF('https://adilsonv77.github.io/meuteste/wizard_test.glb');
-    raccoon.scene.scale.set(30, 30, 30);
+    const raccoon = await loadGLTF('https://adilsonv77.github.io/ar/assets/entity-2.glb');
+   // raccoon.scene.scale.set(30, 30, 30);
+    raccoon.scene.scale.set(0.1, 0.1, 0.1);
     raccoon.scene.position.set(0, -0.4, 0);
 
     const anchor = mindarThree.addAnchor(0);
